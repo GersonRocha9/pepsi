873ca079a3e25d6aac53510a8bd01abeeaab61a6
Gerson Rocha <gersonrocha9@gmail.com>
2022-03-25 16:16:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(pepsi) {
+  document.querySelector(".pepsi").src = pepsi;
+}
+
+function changeBgColor(bgColor) {
+  const sec = document.querySelector(".sec");
+  sec.style.backgroundColor = bgColor;
+}
